c84d5ad621d1760ef01b7306c19b9d3198f97719
brianWelman2 <brianallisterwelman@gmail.com>
2021-02-17 20:29:56 +0200

    Added sky-model file paths as attributes

--- kalcal/datasets/sky_models/__init__.py
+++ kalcal/datasets/sky_models/__init__.py
@@ -1,1 +1,11 @@
+# kalcal.dataset.sky_models attributes
+from pkg_resources import resource_filename
 
+MODEL_1 = resource_filename('kalcal', 
+            'datasets/sky_models/MODEL-1.txt')
+
+MODEL_4 = resource_filename('kalcal', 
+            'datasets/sky_models/MODEL-4.txt')
+
+MODEL_50 = resource_filename('kalcal', 
+            'datasets/sky_models/MODEL-50.txt')

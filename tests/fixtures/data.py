6baf93c6af180800eb2012ad4a6d94d7c5e6dfa8
brianWelman2 <brianallisterwelman@gmail.com>
2021-02-25 11:01:46 +0200

    Model data generation

--- tests/fixtures/data.py
+++ tests/fixtures/data.py
@@ -1,53 +1,68 @@
 import pytest
 import numpy as np
-from daskms import xds_from_table
+import Tigger
+from africanus.coordinates import radec_to_lm
+from africanus.dft.kernels import im_to_vis
+from africanus.model.coherency.conversion import convert
 
-@pytest.fixture
-def ms_dir(tmp_path_factory):
-    return 
-
-@pytest.fixture
-def time_variables(
-    n_time,
-    n_ant
-):
-    """Create the time-bin indices and counts arrays
-    that would be extracted from an MS."""
-    tbin_indices = np.zeros(n_time, dtype=np.int32)
-    tbin_counts = np.zeros(n_time, dtype=np.int32)
-
-    n_bl = n_ant * (n_ant - 1)//2
-
-    for t in range(n_time):
-        tbin_indices[t] = t * n_bl
-        tbin_counts[t] = n_bl
-
-    return tbin_indices, tbin_counts
+from .ms import (sel_PHASE_DIR, sel_FREQ, 
+                    sel_UVW, n_chan, n_row)
+from .lsm import sel_LSM, n_dir
 
 
-@pytest.fixture
-def antenna_pairs(
-    n_time,
-    n_ant,
-    half='upper'
-):
-    """Create antenna pair arrays based on the
-    upper or lower triangle indices."""
+@pytest.fixture(scope="module")
+def gen_lm(sel_PHASE_DIR, sel_LSM, n_dir):
 
-    index_func = None
-    k = 0
-    if half == 'upper':
-        index_func = np.triu_indices
-        k = 1
-    elif half == 'lower':
-        index_func = np.tril_indices
+    lm = np.zeros((n_dir, 2), dtype=np.float64)
 
-    ant1 = []
-    ant2 = []
-    for p, q in zip(*index_func(n_ant, k)):
-        if p != q:
-            ant1.append(p)
-            ant2.append(q)
-        
-    return (np.tile(ant1, n_time), 
-        np.tile(ant2, n_time))
+    # Cycle coordinates creating a source with flux
+    for d, source in enumerate(sel_LSM.sources):
+
+        # Extract position
+        radec_s = np.array([[source.pos.ra, source.pos.dec]])
+        lm[d] =  radec_to_lm(radec_s, sel_PHASE_DIR)
+
+    return lm
+
+
+@pytest.fixture(scope="module")
+def gen_MODEL_DATA(sel_FREQ, sel_UVW, sel_LSM, 
+                    gen_lm, n_chan, n_row, n_dir):
+
+    # Create initial model array
+    model = np.zeros((n_dir, n_chan, 4), dtype=np.float64)
+
+    # Cycle coordinates creating a source with flux
+    for d, source in enumerate(sel_LSM.sources):
+        flux = getattr(source, 'flux')
+        for i, stokes in enumerate(['I', 'Q', 'U', 'V']):
+            stokes_val = getattr(flux, stokes)
+            if stokes_val:
+                # Get spectrum (only spi currently supported)
+                tmp_spec = source.spectrum
+                spi = [tmp_spec.spi if tmp_spec 
+                            is not None else 0.0]
+                ref_freq = [tmp_spec.freq0 if tmp_spec 
+                                    is not None else 1.0]
+    
+                # Generate model flux
+                model[d, :, i] = stokes_val\
+                    * (sel_FREQ/ref_freq)**spi
+
+    # Get model visibilities
+    model_vis = np.zeros((n_row, n_chan, n_dir, 4), 
+                            dtype=np.complex128)
+    for s in range(n_dir):
+        model_vis[:, :, s] = im_to_vis(
+            model[s].reshape((1, n_chan, 4)),
+            sel_UVW, 
+            gen_lm[s].reshape((1, 2)), 
+            sel_FREQ, 
+            dtype=np.complex64)
+
+    # Convert Stokes to corr
+    in_schema = ['I', 'Q', 'U', 'V']
+    out_schema = [['RR', 'RL'], ['LR', 'LL']]
+    model_vis = convert(model_vis, in_schema, out_schema)
+
+    return model_vis.reshape(n_row, n_chan, n_dir, 4)

73e1fe892ae17adb70b642e3d64cd8b88ebcad89
brianWelman2 <brianallisterwelman@gmail.com>
2021-03-02 19:00:44 +0200

    Calculates per parameter

--- kalcal/tools/statistics.py
+++ kalcal/tools/statistics.py
@@ -1,22 +1,37 @@
 import numpy as np
 
+from kalcal.tools.utils import diag_cov_reshape, measure_vector
+
+
+def average(m):
+
+    n_time, n_ant, n_chan, n_dir, _ = m.shape
+
+    N = 2 * n_time * n_ant * n_chan * n_dir
+
+    return 1/N * np.sum(m)
+    
 
 def sigma_test(states, true_jones, cov, n):
     
     n_time, n_ant, n_chan, n_dir = true_jones.shape 
+
     aug_true_jones = np.stack((true_jones, 
                             true_jones.conj()), axis=4)
-    stats = np.zeros(n_time)
-    m = 2 * n_ant * n_chan * n_dir
-    
-    for t in range(n_time):
-        x = states[t].ravel()
-        std = np.sqrt(np.diag(cov[t]))
-        mu = aug_true_jones[t].ravel()
 
-        LB = x - n * std
-        UB = x + n * std
+    stats = np.zeros_like(states, dtype=np.int16)
+    shape = (n_ant, n_chan, n_dir, 2)
+    for t in range(n_time):        
+        std = np.sqrt(diag_cov_reshape(cov[t], shape))    
+
+        for a in range(n_ant):
+            for c in range(n_chan):
+                for s in range(n_dir): 
+                    for i in range(2):
+                        x = states[t, a, c, s, i]
+                        mu = aug_true_jones[t, a, c, s, i]
+                        LB = x - n * std[a, c, s, i]
+                        UB = x + n * std[a, c, s, i]
+                        stats[t, a, c, s, i] = ((LB <= mu) & (mu <= UB))
         
-        stats[t] = 1/m * np.sum((LB <= mu) & (mu <= UB))
-        
-    return 1/n_time * np.sum(stats)
+    return stats

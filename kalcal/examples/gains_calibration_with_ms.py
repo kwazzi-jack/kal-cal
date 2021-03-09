872dcfc77e5972e22e9fd1abd6293063b05b5b8d
brianWelman2 <brianallisterwelman@gmail.com>
2021-03-09 14:30:51 +0200

    Magnitude test implemented with tabulate

--- kalcal/examples/gains_calibration_with_ms.py
+++ kalcal/examples/gains_calibration_with_ms.py
@@ -6,11 +6,13 @@ from kalcal.generation import from_ms
 from kalcal.generation import create_ms
 from kalcal.generation import loader
 from kalcal.plotting.multiplot import plot_time
-from kalcal.tools.statistics import sigma_test
+from kalcal.tools.statistics import (state_sigma_test,
+    magnitude_sigma_test)
 
 import matplotlib.pyplot as plt
 import numpy as np
 from os import path
+from tabulate import tabulate
 
 
 def main():   
@@ -66,21 +68,38 @@ def main():
     ms, Ps, G = ext_kalman_smoother(m, P, Q)
 
     n_states = 2 * n_time * n_ant * n_chan * n_dir
-    F1 = np.mean(sigma_test(m, jones, P, 1))
-    S1 = np.mean(sigma_test(ms, jones, Ps, 1))
-    print(f"==> 1-sigma test | FILTER | {n_states} states : {F1*100:.2f} %")
-    print(f"==> 1-sigma test | SMOOTH | {n_states} states : {S1*100:.2f} %")
+    SF1 = np.mean(state_sigma_test(m, jones, P, 1))*100
+    SS1 = np.mean(state_sigma_test(ms, jones, Ps, 1))*100
+    SF2 = np.mean(state_sigma_test(m, jones, P, 2))*100
+    SS2 = np.mean(state_sigma_test(ms, jones, Ps, 2))*100
+    SF3 = np.mean(state_sigma_test(m, jones, P, 3))*100
+    SS3 = np.mean(state_sigma_test(ms, jones, Ps, 3))*100
 
-    F2 = np.mean(sigma_test(m, jones, P, 2))
-    S2 = np.mean(sigma_test(ms, jones, Ps, 2))
-    print(f"==> 2-sigma test | FILTER | {n_states} states : {F2*100:.2f} %")
-    print(f"==> 2-sigma test | SMOOTH | {n_states} states : {S2*100:.2f} %")
+    S1 = ['1', f"{SF1:.2f}%", f"{SS1:.2f}%", n_states]
+    S2 = ['2', f"{SF2:.2f}%", f"{SS2:.2f}%", n_states]
+    S3 = ['3', f"{SF3:.2f}%", f"{SS3:.2f}%", n_states]
+    S = [S1, S2, S3]
 
-    F3 = np.mean(sigma_test(m, jones, P, 3))
-    S3 = np.mean(sigma_test(ms, jones, Ps, 3))
-    print(f"==> 3-sigma test | FILTER | {n_states} states : {F3*100:.2f} %")
-    print(f"==> 3-sigma test | SMOOTH | {n_states} states : {S3*100:.2f} %")
-    
+    print("==> State Sigma-tests")
+    print(tabulate(S, headers=['Sigmas', 'Filter', 'Smoother', 'States'], 
+                    tablefmt='fancy_grid'))
+
+    MF1 = np.mean(magnitude_sigma_test(m, jones, P, 1))*100
+    MS1 = np.mean(magnitude_sigma_test(ms, jones, Ps, 1))*100
+    MF2 = np.mean(magnitude_sigma_test(m, jones, P, 2))*100
+    MS2 = np.mean(magnitude_sigma_test(ms, jones, Ps, 2))*100
+    MF3 = np.mean(magnitude_sigma_test(m, jones, P, 3))*100
+    MS3 = np.mean(magnitude_sigma_test(ms, jones, Ps, 3))*100
+
+    M1 = ['1', f"{MF1:.2f}%", f"{MS1:.2f}%", n_states]
+    M2 = ['2', f"{MF2:.2f}%", f"{MS2:.2f}%", n_states]
+    M3 = ['3', f"{MF3:.2f}%", f"{MS3:.2f}%", n_states]
+    M = [M1, M2, M3]
+
+    print("==> Magnitude Sigma-tests")
+    print(tabulate(M, headers=['Sigmas', 'Filter', 'Smoother', 'States'], 
+                    tablefmt='fancy_grid'))
+
     show = [1, 2, 3]
     plot_time(
         jones, 'True Jones', '-',

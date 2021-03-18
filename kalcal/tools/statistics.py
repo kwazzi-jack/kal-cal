ef261cf18a73ad626b5e3faa4e512202c9db4c0a
brianWelman2 <brianallisterwelman@gmail.com>
2021-03-18 23:35:15 +0200

    Added SSE, MSE and condition number tests

--- kalcal/tools/statistics.py
+++ kalcal/tools/statistics.py
@@ -57,3 +57,39 @@ def magnitude_sigma_test(states, true_jones, cov, n=1, ref_ant=0):
                             ((LB <= mu) & (mu <= UB))
         
     return stats
+
+
+def state_SSE(states, true_jones):
+
+    n_time, n_ant, n_chan, n_dir = true_jones.shape
+
+    aug_true_jones = np.stack((true_jones, 
+                            true_jones.conj()), axis=4)
+
+    SSE = 0.0
+    for t in range(n_time):        
+        for a in range(n_ant):
+            for c in range(n_chan):
+                for s in range(n_dir): 
+                    for i in range(2):
+                        e = states[t, a, c, s, i]\
+                            - aug_true_jones[t, a, c, s, i]
+                        
+                        SSE += np.power(np.abs(e), 2)
+
+    return SSE
+
+
+def state_MSE(states, true_jones):
+
+    n = states.size
+    SSE = state_SSE(states, true_jones)
+
+    return SSE/n
+
+
+def cond_number(P):
+    eigenvals = np.linalg.eigvals(P)
+    Lmin = np.min(eigenvals).real
+    Lmax = np.max(eigenvals).real
+    return (Lmax/Lmin)

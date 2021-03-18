aee21a0c8dd53115f1fddaec75a2649e7f54fb0f
brianWelman2 <brianallisterwelman@gmail.com>
2021-03-18 23:32:49 +0200

    Remove large venv folder to reduce install size

--- venv/lib/python3.6/site-packages/casacore/quanta/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._quanta import *
-
-from .quantity import quantity, is_quantity
-
-constants = constants()
-units = units()
-prefixes = prefixes()
-del Quantity, QuantVec, from_string, from_dict_v
--- venv/lib/python3.6/site-packages/tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-import Kittens.utils
-
-
-class TestUtils(TestCase):
-    def test_pyfits(self):
-        Kittens.utils.import_pyfits()

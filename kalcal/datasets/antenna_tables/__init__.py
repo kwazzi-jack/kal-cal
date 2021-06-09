d9625bae6a09a4123e9ca6a6bf1e9886003e7717
brianWelman2 <brianallisterwelman@gmail.com>
2021-06-09 14:05:13 +0200

    Added other antenna tables for create empty ms function

--- kalcal/datasets/antenna_tables/__init__.py
+++ kalcal/datasets/antenna_tables/__init__.py
@@ -1,6 +1,46 @@
 # kalcal.dataset.sky_models attributes
 from pkg_resources import resource_filename
 
-KAT7 = resource_filename('kalcal', 
-            'datasets/antenna_tables/'\
-            + 'kat-7.itrf.txt')
+# Antenna tables made by Sphe on `simms` package (KERN)
+# See: https://github.com/ratt-ru/simms/tree/master/simms/observatories
+
+KAT7 = resource_filename(
+    'kalcal', 'datasets/antenna_tables/kat-7.itrf.txt')
+
+LOFAR = resource_filename(
+    'kalcal', 'datasets/antenna_tables/lofar_ml.itrf.txt')
+
+MEERKAT = resource_filename(
+    'kalcal', 'datasets/antenna_tables/meerkat.itrf.txt'))
+
+SKA197 = resource_filename(
+    'kalcal', 'datasets/antenna_tables/skamid197.itrf.txt')
+
+SKA254 = resource_filename(
+    'kalcal', 'datasets/antenna_tables/skamid254.itrf.txt')
+
+VLAA = resource_filename(
+    'kalcal', 'datasets/antenna_tables/vlaa.itrf.txt')
+
+VLAB = resource_filename(
+    'kalcal', 'datasets/antenna_tables/vlab.itrf.txt')
+
+VLAC = resource_filename(
+    'kalcal', 'datasets/antenna_tables/vlac.itrf.txt')
+
+VLAD = resource_filename(
+    'kalcal', 'datasets/antenna_tables/vlad.itrf.txt')
+
+WSRT = resource_filename(
+    'kalcal', 'datasets/antenna_tables/wsrt.itrf.txt')
+
+
+def list_antenna_tables():
+    """List all antenna tables allowed for empty ms creation."""
+
+    output = """
+    ===> Below are implemented antenna-tables for `create_empty_ms` command:
+    ===> kat7, lofar, meerkat, ska197, ska254, vlaa, vlab, vlac, vlad, wsrt   
+    """
+
+    print(output)

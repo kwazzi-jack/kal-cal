9831d223dcd9aae84a36bc52ffc52fe188262606
Brian Welman <brianallisterwelman@gmail.com>
2022-02-22 22:47:00 +0200

    Adjusted to account for single correlations

--- kalcal/calibration/vanilla.py
+++ kalcal/calibration/vanilla.py
@@ -2,7 +2,7 @@ from omegaconf import OmegaConf as ocf
 from kalcal.filters import ekf
 from kalcal.smoothers import eks
 from kalcal.tools.utils import gains_vector, concat_dir_axis
-from dask import array as da 
+from dask import array as da
 from daskms import xds_from_ms, xds_to_table
 from africanus.calibration.utils import corrupt_vis, correct_vis
 import numpy as np
@@ -19,8 +19,8 @@ def calibrate(msname, **kwargs):
     # Options to attributed dictionary
     if kwargs["yaml"] is not None:
         options = ocf.load(kwargs["yaml"])
-    else:    
-        options = ocf.create(kwargs)    
+    else:
+        options = ocf.create(kwargs)
 
     # Set to struct
     ocf.set_struct(options, True)
@@ -41,7 +41,7 @@ def calibrate(msname, **kwargs):
         "numba" : ekf.numba_algorithm,
         "sparse" : ekf.sparse_algorithm
     }[options.algorithm.lower()]
-    
+
     # Choose smoother algorithm
     kalman_smoother = eks.numba_algorithm
 
@@ -53,9 +53,9 @@ def calibrate(msname, **kwargs):
     n_row = dims.row
     n_chan = dims.chan
     n_corr = dims.corr
-    
+
     # Check if single or multiple model columns
-    model_columns = options.model_column.replace(" ", "").split(",")    
+    model_columns = options.model_column.replace(" ", "").split(",")
 
     # Load model visibilities (dask ignored for now)
     model = concat_dir_axis(MS, model_columns).compute().astype(np.complex128)
@@ -65,14 +65,14 @@ def calibrate(msname, **kwargs):
 
     # Load data visibilities (dask ignored for now)
     vis = MS.get(options.vis_column).data.compute().astype(np.complex128)
-    
+
     # Load weights (dask ignored for now)
     weight = MS.get(options.weight_column).data.compute().astype(np.complex128)
 
     # Get time-bin indices and counts
     _, tbin_indices, tbin_counts = np.unique(MS.TIME,
-                                        return_index=True, 
-                                        return_counts=True)   
+                                        return_index=True,
+                                        return_counts=True)
 
     # Get antenna arrays (dask ignored for now)
     ant1 = MS.ANTENNA1.data.compute()
@@ -83,7 +83,7 @@ def calibrate(msname, **kwargs):
 
     # Get flag column
     flag = MS.FLAG.data.compute()
-    
+
     # Set time dimension
     n_time = len(tbin_indices)
 
@@ -94,14 +94,10 @@ def calibrate(msname, **kwargs):
     s_model = model[s_row, s_chan, s_dir]
 
     # Check correlation axis size
-    if s_model.shape[0] != 4:
-        raise NotImplementedError("Only uses (4,) correlation shape.")
-
-    # Find mode to use
     if s_model.all():
         # All correlations
         corr = [0, 1, 2, 3]
-        
+
     elif s_model[[0, 3]].all():
         # First and last correlations
         corr = [0, 3]
@@ -113,7 +109,7 @@ def calibrate(msname, **kwargs):
     else:
         # (2 x 2) not implemented yet
         raise ValueError("Cannot identify correlation shape.")
-    
+
     # Gains solutions
     filter_gains = np.zeros((n_time, n_ant, n_chan, n_dir, n_corr, 2),
                                 dtype=np.complex128)
@@ -125,7 +121,7 @@ def calibrate(msname, **kwargs):
     if options.sigma_n is not None:
         cvar = 2 * options.sigma_n**2
         weight = 1.0/cvar * np.ones_like(weight)
-    
+
     for c in corr:
         # Get data related to correlation
         cmodel = model[..., c]
@@ -140,22 +136,22 @@ def calibrate(msname, **kwargs):
         # Noise Matrices
         Q = 2 * options.sigma_f * np.eye(mp.size, dtype=np.complex128)
         R = 2 * options.sigma_n\
-            * np.eye(n_ant * (n_ant - 1) * n_chan, dtype=np.complex128) 
+            * np.eye(n_ant * (n_ant - 1) * n_chan, dtype=np.complex128)
 
         # Variable to keep track of algorithm direction
         a_dir = FORWARD
 
         for i in range(options.filter):
-            m, P = kalman_filter(mp, Pp, cmodel, cvis, cweight, Q, R, 
-                                    ant1, ant2, tbin_indices, 
-                                    tbin_counts, options.step_control)        
+            m, P = kalman_filter(mp, Pp, cmodel, cvis, cweight, Q, R,
+                                    ant1, ant2, tbin_indices,
+                                    tbin_counts, options.step_control)
 
             # Correct flipping if last iteration
             if i == options.filter - 1:
                 if a_dir == BACKWARD:
                     # Reset algorithm direction
                     a_dir = FORWARD
-                    
+
                     # Flip arrays
                     m = m[::-1]
                     P = P[::-1]
@@ -170,7 +166,7 @@ def calibrate(msname, **kwargs):
                     a_dir = BACKWARD
                 elif a_dir == BACKWARD:
                     a_dir = FORWARD
-                
+
                 # Flip arrays
                 mp = gains_vector(m[-1])
                 Pp = P[-1]
@@ -178,21 +174,21 @@ def calibrate(msname, **kwargs):
                 vis = vis[::-1]
                 weight = weight[::-1]
                 tbin_indices = tbin_indices[::-1] # For consistency
-                tbin_counts   
+                tbin_counts
 
         # Save filter gains
         filter_gains[..., c, :] = m.copy()
 
         # Run Kalman Smoother for requested number of times
         for i in range(options.smoother):
-            ms, Ps, _ = kalman_smoother(m, P, Q)        
+            ms, Ps, _ = kalman_smoother(m, P, Q)
 
             # Correct flipping if last iteration
             if i == options.smoother - 1:
                 if a_dir == BACKWARD:
                     # Reset algorithm direction
                     a_dir = FORWARD
-                    
+
                     # Flip arrays
                     ms = ms[::-1]
                     Ps = Ps[::-1]
@@ -202,7 +198,7 @@ def calibrate(msname, **kwargs):
                     a_dir = BACKWARD
                 elif a_dir == BACKWARD:
                     a_dir = FORWARD
-                
+
                 # Flip arrays
                 m = ms[::-1]
                 P = Ps[::-1]
@@ -218,20 +214,20 @@ def calibrate(msname, **kwargs):
 
         # Correct Visibilties
         corrected_data = correct_vis(
-            tbin_indices, 
-            tbin_counts, 
-            ant1, 
-            ant2, 
-            jones, 
-            vis, 
+            tbin_indices,
+            tbin_counts,
+            ant1,
+            ant2,
+            jones,
+            vis,
             flag)
-        
+
         # To dask
         corrected_data = da.from_array(corrected_data,
                             chunks=MS.get(options.vis_column).chunks)
-        
+
         # Assign and write to ms
-        MS = MS.assign(**{options.out_data: (("row", "chan", "corr"), 
+        MS = MS.assign(**{options.out_data: (("row", "chan", "corr"),
                     corrected_data.astype(np.complex64))})
 
         columns = [options.out_data]
@@ -245,13 +241,13 @@ def calibrate(msname, **kwargs):
             weight_spectrum = da.from_array(weight_spectrum,
                             chunks=MS.get(options.vis_column).chunks)
 
-            MS = MS.assign(**{options.out_weight: (("row", "chan", "corr"), 
+            MS = MS.assign(**{options.out_weight: (("row", "chan", "corr"),
                     weight_spectrum.astype(np.float32))})
 
             columns.append(options.out_weight)
 
-        xds_to_table(MS, msname, columns).compute()
-    
+        xds_to_table(MS, msname, columns).write()
+
     # Output filter gains to npy file
     if options.out_filter is not None and options.out_filter != "":
         with open(options.out_filter, "wb") as file:
